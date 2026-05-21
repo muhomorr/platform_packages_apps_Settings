335015939hi/dumbos-platform_packages_apps_Settings

Update utility methods with some full users supporting some profile types
Secondary users now can have private profiles as its nested profile.

src/com/android/settings/Utils.java
@@ -431,6 +431,12 @@ public final class Utils extends com.android.settingslib.Utils {
             if (Objects.equals(umUserType, userInfo.userType)) {
                 return profile;
             }
+            if (userType == ProfileType.PERSONAL) {
+                if (userInfo.canHaveProfileOfType(USER_TYPE_PROFILE_PRIVATE)
+                        || userInfo.canHaveProfileOfType(USER_TYPE_PROFILE_MANAGED)) {
+                    return profile;
+                }
+            }
         }
         return null;
     }
@@ -446,6 +452,12 @@ public final class Utils extends com.android.settingslib.Utils {
         for (UserInfo profile : userProfiles) {
             if (Objects.equals(umUserType, profile.userType)) {
                 return true;
+            }
+            if (userType == ProfileType.PERSONAL) {
+                if (profile.canHaveProfileOfType(USER_TYPE_PROFILE_PRIVATE)
+                        || profile.canHaveProfileOfType(USER_TYPE_PROFILE_MANAGED)) {
+                    return true;
+                }
             }
         }
         return false;

muhomorr/platform_packages_apps_Settings

src/com/android/settings/Utils.java
@@ -454,6 +454,12 @@ public final class Utils extends com.android.settingslib.Utils {
             if (Objects.equals(umUserType, userInfo.userType)) {
                 return profile;
             }
+            if (userType == ProfileType.PERSONAL) {
+                if (userInfo.canHaveProfile(USER_TYPE_PROFILE_PRIVATE)
+                        || userInfo.canHaveProfile(USER_TYPE_PROFILE_MANAGED)) {
+                    return profile;
+                }
+            }
         }
         return null;
     }
@@ -469,6 +475,12 @@ public final class Utils extends com.android.settingslib.Utils {
         for (UserInfo profile : userProfiles) {
             if (Objects.equals(umUserType, profile.userType)) {
                 return true;
+            }
+            if (userType == ProfileType.PERSONAL) {
+                if (profile.canHaveProfile(USER_TYPE_PROFILE_PRIVATE)
+                        || profile.canHaveProfile(USER_TYPE_PROFILE_MANAGED)) {
+                    return true;
+                }
             }
         }
         return false;